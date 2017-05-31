6f6f59e5ef5b641b63d25ed1851d43e7c50634c7
Sushruth Sastry <shastry.sushruth@gmail.com>
2017-05-31 07:48:54 -0400

    Improvements to map and text

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -6,13 +6,13 @@ export class Intro extends Component {
 		return (
 			<div className="flexParent intro">
 				<div className="left flexParent col shrinker start">
-					<div className="assetHeading">Come Let's Grow !!</div>
+					<div className="assetHeading">Come, Let's Grow!</div>
 					<p className="assetBody">
 						One stop destination for all financial solutions as in stock market, Loans, Insurance, Tax, Planning and Other Services. Earn while you learn. Live trading room facility available, First of its kind in India.
 					</p>
 				</div>
 				<div className="right">
-					<img src={require('./../../images/bull-bear.png')} alt="Stock Market" height="130px"/>
+					<img src={require('./../../images/bull-bear.png')} alt="Stock Market" className="introImage"/>
 				</div>
 			</div>
 		)
